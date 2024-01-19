b161407103da3550995a1af6ef90ce1e7103fc3f
faizan khan <faizankhaan27@gmail.com>
2024-01-19 05:58:27 +0500

    completed

--- gaming-website/resources/views/games.blade.php
+++ gaming-website/resources/views/games.blade.php
@@ -116,7 +116,7 @@
   </section>
 
   <!-- Games Section -->
-  <section class="top-games mt-5">
+  <section class="top-games mt-5 mb-5">
     <div class="container mt-5 t-g">
       <div class="row g-5">
   
@@ -205,19 +205,6 @@
   
     </div>
  
-
-
-
-    <div class="container d-flex justify-content-center game-pages  mt-5 mb-5">
-
-      <ul class="pagination">
-        <li class="page-item "><a class="page-link" href="#">Previous</a></li>
-        <li class="page-item  active"><a class="page-link" href="#">1</a></li>
-        <li class="page-item"><a class="page-link" href="#">2</a></li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item"><a class="page-link" href="#">Next</a></li>
-      </ul>
-    </div>
 
 
 
